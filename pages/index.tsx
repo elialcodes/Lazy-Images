7daad5fb60f9d9ac84b6ce9b11b676abf74edc45
Noelia <abascalcabreran@gmail.com>
2024-05-21 16:57:29 +0200

    typing events

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
-import type { NextPage } from 'next';
+import type { NextPage } from 'next'; //nos importamos este el tipado que nos da Next
+import type { MouseEventHandler } from 'react'; //nos importamos este tipado que nos da React para el evento onClick
 import { useState } from 'react';
 import Head from 'next/head';
 import { RandomFox } from '../components/RandomFox';
@@ -10,32 +11,41 @@ const Home: NextPage = () => {
     return Math.floor(Math.random() * 123) + 1;
   };
 
-  //constantes que pasaremos al componente RandomFox por Props:
+  //props que pasaremos al componente RandomFox:
 
   //prop 1:
-  //simulamos como vendrían los datos desde una api en la vida real, en formato array de objetos (con id, url...)
+  //simularemos como vendrían los datos desde una api en la vida real, en formato array de objetos (con id, url...)
   //como no tenemos id, lo generamos:
 
   const generateId = (): string => {
     return Math.random().toString(36).substr(2, 9);
   };
 
-  //establecemos nuestro tipado para cada objeto:
+  //establecemos nuestro tipado para cada objeto del array que nos devolvería la api
 
   type ImageType = { id: string; url: string };
 
-  //creamos una variable de estado que tendrá un array de 4 objetos aleatorios
-  //añadimos al estado <ImageType []> para asegurarnos que el seteo de la variable solo acepte array de objetos
-  //que tengan nuestro tipado
-  const [images, setImages] = useState<ImageType[]>([
-    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
-    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
-    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
-    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
-  ]);
+  //creamos una variable de estado y añadimos al estado <ImageType []> para asegurarnos
+  //que el seteo de la variable solo acepte dentro del array objetos que tengan nuestro tipado
+  const [images, setImages] = useState<ImageType[]>([]);
 
   //prop 2:
   const altImage: string = 'Fox image';
+
+  //función manejadora para añadir una imagen cada vez que hacemos click en un botón:
+  //en el archivo tsconfig.json, tenemos la librería "dom" instalada, así que sólo tenemos que hacer hover
+  //en el evento onClick de abajo y ver qué tipado tenemos que poner,
+  //en este caso es MouseEventHandler<HTMLButtonElement>
+  //esto nos dice que el elemento botón del HTML espera un MouseEventHandler
+  //el tipado MouseEventHandler le importamos arriba, nos lo da la propia librería react
+  //a la constante newFoxImage le daremos el tipado que hemos diseñado
+  const addNewFox: MouseEventHandler<HTMLButtonElement> = () => {
+    const newFoxImage: ImageType = {
+      id: generateId(),
+      url: `https://randomfox.ca/images/${random()}.jpg`,
+    };
+    setImages([...images, newFoxImage]); //seteamos la variable de estado para ir añadiendo imágenes al array
+  };
 
   return (
     <div>
@@ -46,8 +56,9 @@ const Home: NextPage = () => {
 
       <main>
         <h1 className="text-3xl font-bold underline">Foxes images</h1>
-        {/* tomamos la variable de estado images (array de objetos) y renderizamos con un map 
-        el componete RandomFox, al que pasamos props*/}
+        <button onClick={addNewFox}>Add new fox</button>
+        {/* tomamos la variable de estado images (array de objetos) y con cada iteración
+        //de map renderizamos el componete RandomFox, al que pasamos props*/}
         {images.map((image) => (
           <RandomFox key={image.id} url={image.url} alt={altImage} />
         ))}
