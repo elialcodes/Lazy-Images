79b7d1636a27e7df71c94301f3cf17b36facd8e2
Noelia <abascalcabreran@gmail.com>
2024-05-21 20:55:12 +0200

    useRef

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,14 +1,20 @@
+import { useRef } from 'react'; //importamos useRef de la librería de React, nos servirá
+//para saber la posición de la imagen dentro del viewport
+
 //las props que recibe un componene siempre son un objeto
-//diseñamos nuesto propio tipado "Props" para tipar las props que recibe el componente
+//diseñamos nuestro propio tipado "Props" para tipar las props que recibe el componente
 type Props = { key: string; url: string; alt: string };
 
 //pasamos las props con destructuring y le decimos que tendrán el tipado definido en Props
 //tipamos explícitamente que el retorno de la función será un elemento jsx:
+//con useRef tomamos el elemento del DOM que queremos y lo inicializamos a null, porque
+//no hay imagen hasta que se cargue el componente
 export const RandomFox = ({ key, url, alt }: Props): JSX.Element => {
+  const node = useRef<HTMLImageElement>(null);
   return (
     //cada foto será un div con un id único y un padding de 4
     <div key={key} className="p-4">
-      <img width={320} height="auto" src={url} alt={alt} />
+      <img ref={node} width={320} height="auto" src={url} alt={alt} />
     </div>
   );
 };
