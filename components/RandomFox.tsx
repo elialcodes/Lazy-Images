0f9e7a10d28ec3b1b0389dd42b0069f604bea268
Noelia <abascalcabreran@gmail.com>
2024-05-21 11:59:47 +0200

    useState with Typescript

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,9 +1,9 @@
 //las props que recibe un componene siempre son un objeto
 //diseñamos nuesto propio tipado "Props" para tipar las props que recibe el componente
-type Props = { src: string; alt: string };
+type Props = { image: string; alt: string };
 
 //pasamos las props con destructuring y le decimos que tendrán el tipado definido en Props
 //tipamos explícitamente que el retorno de la función será un elemento jsx:
-export const RandomFox = ({ src, alt }: Props): JSX.Element => {
-  return <img width={320} height="auto" src={src} alt={alt} />;
+export const RandomFox = ({ image, alt }: Props): JSX.Element => {
+  return <img width={320} height="auto" src={image} alt={alt} />;
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next';
+import { useState } from 'react';
 import Head from 'next/head';
 import { RandomFox } from '../components/RandomFox';
 
@@ -9,8 +10,18 @@ const Home: NextPage = () => {
     return Math.floor(Math.random() * 123) + 1;
   };
 
-  //constantes que pasaremos por Props:
-  const srcImage: string = `https://randomfox.ca/images/${random()}.jpg`;
+  //constantes que pasaremos al componente RandomFox por Props:
+
+  //prop 1: creamos una variable de estado que tendrá un array de strings con 4 imagenes aleatorias
+  //añadimos al estado <string []> para asegurarnos que el seteo de la variable solo acepte array de strings
+  const [images, setImages] = useState<string[]>([
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+  ]);
+
+  //prop 2:
   const altImage: string = 'Fox image';
 
   return (
@@ -22,7 +33,12 @@ const Home: NextPage = () => {
 
       <main>
         <h1 className="text-3xl font-bold underline">Foxes images</h1>
-        <RandomFox src={srcImage} alt={altImage} />
+        {/* renderizamos con un map el componete RandomFox y sus props, le daremos un padding de 4 (p-4) */}
+        {images.map((image, index) => (
+          <div key={index} className="p-4">
+            <RandomFox image={image} alt={altImage} />
+          </div>
+        ))}
       </main>
 
       <footer></footer>
