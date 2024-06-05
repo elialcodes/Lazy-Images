3ed84c02e0ffdee2b40df2fe3e5c312b27e5dc56
Noelia <abascalcabreran@gmail.com>
2024-06-05 11:06:07 +0200

    last commit

--- next.config.js
+++ next.config.js
@@ -1,28 +1,28 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   swcMinify: true,
-//   output: 'export',
-//   distDir: 'out',
-//   assetPrefix: './',
-//   images: {
-//     unoptimized: true,
-//   },
-// };
-
-// module.exports = nextConfig;
-
-// next.config.js
-const isProd = process.env.NODE_ENV === 'production';
-
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'export',
   distDir: 'out',
-  assetPrefix: isProd ? './' : '',
+  assetPrefix: './',
   images: {
     unoptimized: true,
   },
-  trailingSlash: true, // Esto ayuda a manejar rutas y archivos correctamente en GitHub Pages
 };
+
+// module.exports = nextConfig;
+
+// // next.config.js
+// const isProd = process.env.NODE_ENV === 'production';
+
+// module.exports = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+//   output: 'export',
+//   distDir: 'out',
+//   assetPrefix: isProd ? './' : '',
+//   images: {
+//     unoptimized: true,
+//   },
+//   trailingSlash: true, // Esto ayuda a manejar rutas y archivos correctamente en GitHub Pages
+// };
